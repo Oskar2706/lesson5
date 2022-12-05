6590534b5b681cb50d39ede868132ce51cb07569
Oskar2706 <alexvaraksa@bk.ru>
2022-12-05 21:04:02 +0300

    hw2

---
+++ src/group1/class2.java
@@ -0,0 +1,18 @@
+package group1;
+
+public class class2 {
+    public static void main(String[] args) {
+        String[][] chessBoard = new String[8][8];
+        for (int i = 0; i < chessBoard.length; i++) {
+            for (int j = 0; j < chessBoard[i].length; j++) {
+                if ((i + j) % 2 == 0) {
+                    chessBoard[i][j] = "W";
+                }else{
+                    chessBoard[i][j] = "B";
+                }
+                System.out.print(chessBoard[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
