22c1b78a9fe35560059fcd25f6c6bdf3716f44d7
Oskar2706 <alexvaraksa@bk.ru>
2022-12-14 21:15:20 +0300

    hw5

--- src/group2/class3.java
+++ src/group2/class3.java
@@ -7,13 +7,12 @@ public class class3 {
                          {6, 8, 5, 5},
                          {4, 5, 6, 9}};
         for (int i = 0 ; i < array.length ; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-            }System.out.print(array[i][i] + " ");
+            System.out.print(array[i][i] + " ");
         }
         System.out.println(" ");
         for (int i = 0 ; i < array.length ; i++) {
-            for (int j = 0; j < array[i].length; j++) {
-            }System.out.print(array[i][array.length -1 - i] + " ");
+            System.out.print(array[i][array.length -1 - i] + " ");
         }
+        System.out.println(" ");
     }
 }
